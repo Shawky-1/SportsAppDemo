d4e756457865f0ce0b582cce12e8c3e9bf8793d2
Bassant <>
2023-02-17 21:02:10 +0300

    favorite view

---
+++ SportsAppDemo/ViewControllers/favoriteViewController.swift
@@ -0,0 +1,48 @@
+//
+//  favoriteViewController.swift
+//  SportsAppDemo
+//
+//  Created by Bassant on 17/02/2023.
+//
+
+import UIKit
+
+class favoriteViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+
+    @IBOutlet weak var favTable: UITableView!
+    
+    var sport = ""
+    var favArray: [String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        favTable.delegate = self
+        favTable.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = favTable.dequeueReusableCell(withIdentifier: "favCell") as! favoriteCell
+        
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SportsAppDemo/Views/favoriteViewController.swift
@@ -0,0 +1,29 @@
+//
+//  favoriteViewController.swift
+//  SportsAppDemo
+//
+//  Created by Bassant on 17/02/2023.
+//
+
+import UIKit
+
+class favoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
