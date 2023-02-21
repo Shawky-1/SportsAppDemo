4b85817e73616f2cbf096b241ccfa6b0c744b94a
Bassant <>
2023-02-21 20:47:58 +0300

    adding Alert & navigation to favorite view controller

--- SportsAppDemo/ViewControllers/favoriteViewController.swift
+++ SportsAppDemo/ViewControllers/favoriteViewController.swift
@@ -6,13 +6,15 @@
 //
 
 import UIKit
+import CoreData
 
 class favoriteViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
     @IBOutlet weak var favTable: UITableView!
     
     var sport = ""
-    var favArray: [String] = []
+    var favArray: [NSManagedObject] = []
+    var coreData = CoreData.coreDataObj
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,8 +23,13 @@ class favoriteViewController: UIViewController, UITableViewDelegate, UITableView
         favTable.dataSource = self
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        favArray = coreData.fetch()
+        favTable.reloadData()
+    }
+    
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 4
+        return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -32,17 +39,53 @@ class favoriteViewController: UIViewController, UITableViewDelegate, UITableView
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = favTable.dequeueReusableCell(withIdentifier: "favCell") as! favoriteCell
         
+        cell.favLabel.text = favArray[indexPath.row].value(forKey: "teamName") as? String
+        
         return cell
     }
+    
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        // Alert
+        
+        //alert controller
+        var alert = UIAlertController(title: "Delete", message: "Delete team from favorite list", preferredStyle: .actionSheet)
+        
+        //actions
+        let deleteAction = UIAlertAction(title: "Delete", style: UIAlertAction.Style.destructive) {
+                UIAlertAction in
+            if editingStyle == UITableViewCell.EditingStyle.delete {
+                var item = self.favArray[indexPath.row]  // Item to be deleted
+                self.favArray.remove(at: indexPath.row)  // delete item from favorite array
+                self.coreData.del(sportName: item.value(forKey: "sportName") as! String, leagueID: item.value(forKey: "leagueID") as! Int, teamName: item.value(forKey: "teamName") as! String)   // delete item from coreData
+                tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.automatic)
+                //self.favTable.reloadData()
+            }
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel)
+        
+        // Add the actions
+        alert.addAction(deleteAction)
+        alert.addAction(cancelAction)
+        
+        // Present the controller
+        self.present(alert, animated: true)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let teamDetails = self.storyboard?.instantiateViewController(withIdentifier: "TeamDetailsViewController") as! TeamDetailsViewController
+
+        teamDetails.sport = favArray[indexPath.row].value(forKey: "sportName") as! String
+        teamDetails.leagueID = favArray[indexPath.row].value(forKey: "leagueID") as! Int
+        teamDetails.teamName = favArray[indexPath.row].value(forKey: "teamName") as! String
+
+        self.navigationController?.pushViewController(teamDetails, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80.0
+    }
 
 }
